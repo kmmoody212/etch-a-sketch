f426e53fe598b12a58cbab75f288f22a7413d4e1
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-05-07 19:15:07 -0400

    Styled page with bg color and h1, added hover effect on grid and sized squares in grid to fit container - KM

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const container = document.querySelector(".container");
 
 function makeGrid(numDivs) {
-  for (let i = 0; i < numDivs; i++) {
+  for (let i = 0; i < numDivs * numDivs; i++) {
     const square = document.createElement("div");
     container.appendChild(square);
+    container.classList.add("container");
     square.classList.add("square");
   }
 }
 
-makeGrid(256);
+makeGrid(16);
