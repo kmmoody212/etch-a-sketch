0d6fa68bd8682ef4dcfd5fd1cfacee821545f7a6
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-05-08 14:53:46 -0400

    Added function to change grid size and a btn for the function, added other btns for future functions - KM

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const container = document.querySelector(".container");
 
+makeGrid(16); //sets a nice default grid 16 for first time when loading the page
+
+// a function that makes a grid
 function makeGrid(numDivs) {
+  //clears the current grid
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  //creates the new grid
   for (let i = 0; i < numDivs * numDivs; i++) {
     const square = document.createElement("div");
+    const size = 500 / numDivs;
+    square.style.width = `${size}px`;
+    square.style.height = `${size}px`;
     container.appendChild(square);
     container.classList.add("container");
     square.classList.add("square");
   }
 }
 
-makeGrid(16);
+//makes the button that is in the HTML be actually clickable and then makes it a window prompt
+const btn = document.querySelector(".changeSize");
+btn.addEventListener("click", () => {
+  let reset = prompt("Please pick a number between 0 and 100", "16"); // returns the number from the prompt as "z" NOW didn't safegaurd against it being an actual number
+  makeGrid(reset); //creates a grid with the number from Z
+});
