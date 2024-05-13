bccba4a73a8e5fe991072c553618260bc0fd9dde
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-05-13 17:08:16 -0400

    Made reset button functional - KM

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const container = document.querySelector(".container");
 
 makeGrid(16); //sets a nice default grid 16 for first time when loading the page
-
 // a function that makes a grid
 function makeGrid(numDivs) {
   //clears the current grid
@@ -21,9 +20,31 @@ function makeGrid(numDivs) {
   }
 }
 
+//makes the boxes remain colored after cursor hovers over
+const fillGrid = document.querySelectorAll(".square");
+function addHoverEvent() {
+  fillGrid.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.classList.add("colored");
+    });
+  });
+}
+
+addHoverEvent();
+
 //makes the button that is in the HTML be actually clickable and then makes it a window prompt
 const btn = document.querySelector(".changeSize");
 btn.addEventListener("click", () => {
-  let reset = prompt("Please pick a number between 0 and 100", "16"); // returns the number from the prompt as "z" NOW didn't safegaurd against it being an actual number
-  makeGrid(reset); //creates a grid with the number from Z
+  let reset = prompt("Please pick a number between 0 and 100", "16");
+
+  makeGrid(reset);
+  //creates a grid with the number from reset
 });
+
+//makes the reset button clickable and clear the current grid
+function resetGrid() {
+  document.querySelectorAll(".square").forEach(function (square) {
+    square.style.backgroundColor = "white";
+    square.classList.remove("colored");
+  });
+}
