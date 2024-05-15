a16874623c25c1d3272c01fef48cfbe5346d3b67
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-05-15 12:52:53 -0400

    Corrected hover affect to work every time grid is reset. Deleted color button options for now. Added perameters to grid size. - KM

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const container = document.querySelector(".container");
 
-makeGrid(16); //sets a nice default grid 16 for first time when loading the page
 // a function that makes a grid
 function makeGrid(numDivs) {
-  //clears the current grid
+  //clears the current grid if any
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
@@ -18,33 +17,38 @@ function makeGrid(numDivs) {
     container.classList.add("container");
     square.classList.add("square");
   }
-}
 
-//makes the boxes remain colored after cursor hovers over
-const fillGrid = document.querySelectorAll(".square");
-function addHoverEvent() {
-  fillGrid.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-      square.classList.add("colored");
+  //function that defines that all the squares created in this run of the makegrid function can have a mouseover effect
+  const fillGrid = document.querySelectorAll(".square");
+  function addHoverEvent() {
+    fillGrid.forEach((square) => {
+      square.addEventListener("mouseover", () => {
+        square.classList.add("colored");
+      });
     });
-  });
+  }
+
+  addHoverEvent(); //adds hoverevent to the squares created in this run of the makegrid function
 }
 
-addHoverEvent();
+makeGrid(16); //sets a nice default grid 16 for first time when loading the page
 
 //makes the button that is in the HTML be actually clickable and then makes it a window prompt
 const btn = document.querySelector(".changeSize");
 btn.addEventListener("click", () => {
-  let reset = prompt("Please pick a number between 0 and 100", "16");
+  let reset = prompt("Please pick a number between 0 and 100", "16"); //sets input of the user to a variable called reset
+
+  while (reset > 100 || reset < 0) {
+    reset = prompt("You must pick a number between 0 and 100! TRY AGAIN", "16");
+  }
 
   makeGrid(reset);
-  //creates a grid with the number from reset
 });
 
 //makes the reset button clickable and clear the current grid
 function resetGrid() {
   document.querySelectorAll(".square").forEach(function (square) {
-    square.style.backgroundColor = "white";
-    square.classList.remove("colored");
+    //picks each square and runs an anyomyous function on those squares
+    square.classList.remove("colored"); //removes the coloured CSS allowing the squares to go back to their default setting of white until they are hovered again and have coloured reapply
   });
 }
